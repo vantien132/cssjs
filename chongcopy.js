e94cc193209159a036bc7a5a2146f1f51206e408
vantien132 <58034093+vantien132@users.noreply.github.com>
2020-04-10 15:41:48 +0700

    Update chongcopy.js

--- chongcopy.js
+++ chongcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://leno.vn'>http://leno.vn</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY: <a href='http://leno.vn'>http://leno.vn</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
